a8f5f12d7ba3cd93ab42475aa772af43bdc8d4a2
Equal <matheus.albino@g.globo>
2025-05-07 13:10:51 -0300

    improve install script

--- install.sh
+++ install.sh
@@ -1,17 +1,23 @@
-#!/usr/bin/env bash
-
-TERMINAL_RC="~/.bashrc"
+TERMINAL_RC_CONTENT='\n# Equal Environment Tool\n
+export EQENV_HOME="$HOME/.equal-env"\n
+[ -s "$EQENV_HOME/bin/eqenv" ] && \. "$EQENV_HOME/bin/eqenv"\n'
 
 if [ -n "$ZSH_VERSION" ]; then
-  TERMINAL_RC="~/.zshrc"
+  touch ~/.zshrc
+
+  if [ -z "$EQENV_HOME" ]; then
+    echo $TERMINAL_RC_CONTENT >> ~/.zshrc
+  fi
+
+  source ~/.zshrc
+elif [ -n "$BASH_VERSION" ]; then
+  touch ~/.bashrc
+
+  if [ -z "$EQENV_HOME" ]; then
+    echo $TERMINAL_RC_CONTENT >> ~/.zshrc
+  fi
+
+  source ~/.bashrc
 fi
 
-if [ -z "$EQENV_HOME" ]; then
-  echo '
-# Equal Environment Tool
-export EQENV_HOME="$HOME/.equal-env"
-[ -s "$EQENV_HOME/bin/eqenv" ] && \. "$EQENV_HOME/bin/eqenv"
-' >> "$TERMINAL_RC"
-fi
-
-. "$TERMINAL_RC"
+echo "Done!"
